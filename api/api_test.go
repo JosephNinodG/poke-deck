3a9fa917b5ede9180ec9a2292af53172137ad00d
Joseph Nino de Guzman <61060613+JosephNinodG@users.noreply.github.com>
2024-11-15 00:18:00 +0000

    Add GetCards endpoint

--- api/api_test.go
+++ api/api_test.go
@@ -113,6 +113,89 @@ func Test_GetCardById(t *testing.T) {
 	}
 }
 
+func Test_GetCards(t *testing.T) {
+	tests := []struct {
+		name                  string
+		request               model.GetCardsRequest
+		queryParam            string
+		httpMethod            string
+		expectedStatusCode    int
+		expectedErrorResponse string
+		expectedResponse      model.PokemonCard
+	}{
+		{
+			name: "Error - Incorrect http method",
+			request: model.GetCardsRequest{
+				Card: model.Card{
+					Name:      "test-name",
+					Type:      "test-type",
+					Supertype: "test-supertype",
+					Subtype:   "test-subtype",
+					Set:       "test-set",
+					Attack:    "test-attack",
+					Legalities: model.Legalities{
+						Standard:  "legal",
+						Expanded:  "legal",
+						Unlimited: "legal",
+					},
+				},
+				Paramters: model.Parameters{
+					MaxCards: 1,
+					OrderBy:  "name",
+				},
+			},
+			queryParam:            "id",
+			httpMethod:            http.MethodDelete,
+			expectedStatusCode:    http.StatusMethodNotAllowed,
+			expectedErrorResponse: "HTTP method not allowed on route. Expected GET",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := httptest.NewRequest(test.httpMethod, "/getcards", nil)
+			w := httptest.NewRecorder()
+			GetCards(w, req)
+			res := w.Result()
+			defer func() {
+				err := res.Body.Close()
+				if err != nil {
+					t.Fatalf("error closing io.ReadCloser for res.Body")
+				}
+			}()
+
+			if res.StatusCode != test.expectedStatusCode {
+				t.Fatalf("expected\n%v\nactual\n%v", res.StatusCode, test.expectedStatusCode)
+			}
+
+			if test.expectedErrorResponse != "" {
+				response, err := getResponseBodyAsString(res.Body)
+				if err != nil {
+					t.Fatalf("error converting response body to string")
+				}
+
+				if test.expectedErrorResponse != response {
+					t.Fatalf("\nexpected\n%v\nactual\n%v", test.expectedResponse, response)
+				}
+			}
+
+			if test.expectedStatusCode == http.StatusOK {
+				var response model.PokemonCard
+
+				err := json.NewDecoder(res.Body).Decode(&response)
+				if err != nil {
+					t.Fatalf("error decoding json body: %v", err)
+				}
+
+				if !reflect.DeepEqual(test.expectedResponse, response) {
+					t.Fatalf("\nexpected\n%v\nactual\n%v", test.expectedResponse, response)
+				}
+			}
+
+		})
+	}
+}
+
 func getResponseBodyAsString(responseBody io.ReadCloser) (string, error) {
 	buf := new(bytes.Buffer)
 	_, err := buf.ReadFrom(responseBody)
@@ -121,117 +204,4 @@ func getResponseBodyAsString(responseBody io.ReadCloser) (string, error) {
 	}
 
 	return buf.String(), nil
-}
-
-var pokemonCard = model.PokemonCard{
-	ID:          "",
-	Name:        "",
-	Supertype:   "",
-	Subtypes:    []string{},
-	Level:       "",
-	Hp:          "",
-	Types:       []string{},
-	EvolvesFrom: "",
-	EvolvesTo:   []string{},
-	Rules:       []string{},
-	AncientTrait: &struct {
-		Name string "json:\"name\""
-		Text string "json:\"text\""
-	}{},
-	Abilities: []struct {
-		Name string "json:\"name\""
-		Text string "json:\"text\""
-		Type string "json:\"type\""
-	}{},
-	Attacks: []struct {
-		Name                string   "json:\"name\""
-		Cost                []string "json:\"cost\""
-		ConvertedEnergyCost int      "json:\"convertedEnergyCost\""
-		Damage              string   "json:\"damage\""
-		Text                string   "json:\"text\""
-	}{},
-	Weaknesses: []struct {
-		Type  string "json:\"type\""
-		Value string "json:\"value\""
-	}{},
-	Resistances: []struct {
-		Type  string "json:\"type\""
-		Value string "json:\"value\""
-	}{},
-	RetreatCost:          []string{},
-	ConvertedRetreatCost: 0,
-	Set: struct {
-		ID           string "json:\"id\""
-		Name         string "json:\"name\""
-		Series       string "json:\"series\""
-		PrintedTotal int    "json:\"printedTotal\""
-		Total        int    "json:\"total\""
-		Legalities   struct {
-			Unlimited string "json:\"unlimited\""
-		} "json:\"legalities\""
-		PtcgoCode   string "json:\"ptcgoCode\""
-		ReleaseDate string "json:\"releaseDate\""
-		UpdatedAt   string "json:\"updatedAt\""
-		Images      struct {
-			Symbol string "json:\"symbol\""
-			Logo   string "json:\"logo\""
-		} "json:\"images\""
-	}{},
-	Number:                 "",
-	Artist:                 "",
-	Rarity:                 "",
-	FlavorText:             "",
-	NationalPokedexNumbers: []int{},
-	Legalities: struct {
-		Unlimited string "json:\"unlimited\""
-	}{},
-	Images: struct {
-		Small string "json:\"small\""
-		Large string "json:\"large\""
-	}{},
-	TCGPlayer: struct {
-		URL       string "json:\"url\""
-		UpdatedAt string "json:\"updatedAt\""
-		Prices    struct {
-			Holofoil *struct {
-				Low    float64 "json:\"low\""
-				Mid    float64 "json:\"mid\""
-				High   float64 "json:\"high\""
-				Market float64 "json:\"market\""
-			} "json:\"holofoil,omitempty\""
-			ReverseHolofoil *struct {
-				Low    float64 "json:\"low\""
-				Mid    float64 "json:\"mid\""
-				High   float64 "json:\"high\""
-				Market float64 "json:\"market\""
-			} "json:\"reverseHolofoil,omitempty\""
-			Normal *struct {
-				Low    float64 "json:\"low\""
-				Mid    float64 "json:\"mid\""
-				High   float64 "json:\"high\""
-				Market float64 "json:\"market\""
-			} "json:\"normal,omitempty\""
-		} "json:\"prices\""
-	}{},
-	CardMarket: struct {
-		URL       string "json:\"url\""
-		UpdatedAt string "json:\"updatedAt\""
-		Prices    struct {
-			AverageSellPrice *float64 "json:\"averageSellPrice\""
-			LowPrice         *float64 "json:\"lowPrice\""
-			TrendPrice       *float64 "json:\"trendPrice\""
-			GermanProLow     *float64 "json:\"germanProLow\""
-			SuggestedPrice   *float64 "json:\"suggestedPrice\""
-			ReverseHoloSell  *float64 "json:\"reverseHoloSell\""
-			ReverseHoloLow   *float64 "json:\"reverseHoloLow\""
-			ReverseHoloTrend *float64 "json:\"reverseHoloTrend\""
-			LowPriceExPlus   *float64 "json:\"lowPriceExPlus\""
-			Avg1             *float64 "json:\"avg1\""
-			Avg7             *float64 "json:\"avg7\""
-			Avg30            *float64 "json:\"avg30\""
-			ReverseHoloAvg1  *float64 "json:\"reverseHoloAvg1\""
-			ReverseHoloAvg7  *float64 "json:\"reverseHoloAvg7\""
-			ReverseHoloAvg30 *float64 "json:\"reverseHoloAvg30\""
-		} "json:\"prices\""
-	}{},
 }
--- tcgapi/tcgapi.go
+++ tcgapi/tcgapi.go
@@ -1,26 +1,14 @@
 package tcgapi
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/JosephNinodG/poke-deck/model"
-	pokemontcgv2 "github.com/PokemonTCG/pokemon-tcg-sdk-go-v2/pkg"
 	"github.com/PokemonTCG/pokemon-tcg-sdk-go-v2/pkg/request"
 )
 
-func GetCard(GetCardRequest model.GetCardRequest) (*pokemontcgv2.PokemonCard, error) {
-	card, err := client.GetCardByID(GetCardRequest.Id)
-	//TODO: Handle specific response codes
-
-	if err != nil {
-		return nil, err
-	}
-
-	return card, nil
-}
-
-func GetCardById(Id string) (model.PokemonCard, error) {
-	card, err := client.GetCardByID(Id)
+func GetCardById(id string) (model.PokemonCard, error) {
+	card, err := client.GetCardByID(id)
 	if err != nil {
 		return model.PokemonCard{}, err
 	}
@@ -34,17 +22,66 @@ func GetCardById(Id string) (model.PokemonCard, error) {
 	return pokemonCard, nil
 }
 
-// Example GetCards func
-func GetCards() {
-	cards, err := client.GetCards(
-		request.Query("name:jirachi", "types:psychic"),
-		request.PageSize(5),
-	)
-	if err != nil {
-		log.Fatal(err)
+func GetCards(req model.GetCardsRequest) ([]model.PokemonCard, error) {
+	if req.Paramters.OrderBy != "" && req.Paramters.Desc {
+		req.Paramters.OrderBy = fmt.Sprintf("-%v", req.Paramters.OrderBy)
 	}
 
-	for _, card := range cards {
-		log.Println(card.Name)
+	cards, err := client.GetCards(
+		request.Query(buildQuery(req.Card)...),
+		request.OrderBy(req.Paramters.OrderBy),
+		request.PageSize(req.Paramters.MaxCards),
+	)
+	if err != nil {
+		return nil, err
 	}
+
+	var pokemonCards []model.PokemonCard
+	for _, card := range cards {
+		pokemonCards = append(pokemonCards, model.PokemonCard(*card))
+	}
+
+	return pokemonCards, nil
+}
+
+func buildQuery(card model.Card) []string {
+	var query []string
+
+	if card.Name != "" {
+		query = append(query, fmt.Sprintf("name:%v", card.Name))
+	}
+
+	if card.Type != "" {
+		query = append(query, fmt.Sprintf("types:%v", card.Type))
+	}
+
+	if card.Supertype != "" {
+		query = append(query, fmt.Sprintf("supertypes:%v", card.Supertype))
+	}
+
+	if card.Subtype != "" {
+		query = append(query, fmt.Sprintf("subtypes:%v", card.Subtype))
+	}
+
+	if card.Set != "" {
+		query = append(query, fmt.Sprintf("set.name:%v", card.Set))
+	}
+
+	if card.Attack != "" {
+		query = append(query, fmt.Sprintf("attacks.name:%v", card.Attack))
+	}
+
+	if card.Legalities.Standard != "" {
+		query = append(query, fmt.Sprintf("legalities.standard:%v", card.Legalities.Standard))
+	}
+
+	if card.Legalities.Expanded != "" {
+		query = append(query, fmt.Sprintf("legalities.expanded:%v", card.Legalities.Expanded))
+	}
+
+	if card.Legalities.Unlimited != "" {
+		query = append(query, fmt.Sprintf("legalities.unlimited:%v", card.Legalities.Unlimited))
+	}
+
+	return query
 }
