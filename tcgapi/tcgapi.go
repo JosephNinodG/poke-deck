4ff0772bed71445b27656ff496cec1e6b68fda20
Joseph Nino de Guzman <61060613+JosephNinodG@users.noreply.github.com>
2024-12-19 02:58:44 +0000

    Build http request for legalities

--- tcgapi/tcgapi.go
+++ tcgapi/tcgapi.go
@@ -1,22 +1,20 @@
 package tcgapi
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"strconv"
 
 	"github.com/JosephNinodG/poke-deck/model"
 	"github.com/PokemonTCG/pokemon-tcg-sdk-go-v2/pkg/request"
 )
 
-//TODO: May have to build entire new api wrapper to handle requests instead of relying on sdk
-// struct provided by sdk is missing legalities.standard and legalities.expanded
-// func GetCardByLegality(queryParams string) {
-// 	ctx := context.Background()
-// 	requestURL := fmt.Sprintf("https://api.pokemontcg.io/v2/cards%v", queryParams)
-// 	res, err := http.Get(requestURL)
-// 	if err != nil {
-// 		slog.ErrorContext(ctx, err.Error())
-// 	}
-// }
+//TODO: Add mapper from pokemon-tcg-sdk-go-v2 to model.PokemonCard to allow Legalities in struct
 
 func GetCardById(id string) (model.PokemonCard, error) {
 	card, err := client.GetCardByID(id)
@@ -33,26 +31,107 @@ func GetCardById(id string) (model.PokemonCard, error) {
 	return pokemonCard, nil
 }
 
-func GetCards(req model.GetCardsRequest) ([]model.PokemonCard, error) {
+func GetCards(req model.GetCardsRequest, apikey string) ([]model.PokemonCard, error) {
 	if req.Paramters.OrderBy != "" && req.Paramters.Desc {
 		req.Paramters.OrderBy = fmt.Sprintf("-%v", req.Paramters.OrderBy)
 	}
 
-	cards, err := client.GetCards(
-		request.Query(buildQuery(req.Card)...),
-		request.OrderBy(req.Paramters.OrderBy),
-		request.PageSize(req.Paramters.MaxCards),
-	)
+	var pokemonCards []model.PokemonCard
+	var err error
+
+	if req.Card.Legalities.Standard != "" || req.Card.Legalities.Expanded != "" {
+		pokemonCards, err = getCardsWithQueryParams(req, apikey)
+		if err != nil {
+			return nil, err
+		}
+
+	} else {
+		cards, err := client.GetCards(
+			request.Query(buildQuery(req.Card)...),
+			request.OrderBy(req.Paramters.OrderBy),
+			request.PageSize(req.Paramters.MaxCards),
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, card := range cards {
+			pokemonCards = append(pokemonCards, model.PokemonCard(*card))
+		}
+	}
+
+	return pokemonCards, nil
+}
+
+func getCardsWithQueryParams(req model.GetCardsRequest, apikey string) ([]model.PokemonCard, error) {
+	queryList := buildQuery(req.Card)
+	queryString := buildQueryString(queryList)
+	//queryParams := addQueryParameters(queryString, req.Paramters)
+
+	ctx := context.Background()
+	request, err := http.NewRequestWithContext(ctx, "GET", "https://api.pokemontcg.io/v2/cards", bytes.NewBuffer([]byte(``)))
 	if err != nil {
 		return nil, err
 	}
 
-	var pokemonCards []model.PokemonCard
-	for _, card := range cards {
-		pokemonCards = append(pokemonCards, model.PokemonCard(*card))
+	request.Header.Add("x-api-key", apikey)
+	values := request.URL.Query()
+	values.Add("q", queryString)
+	values.Add("orderBy", req.Paramters.OrderBy)
+	values.Add("pageSize", strconv.Itoa(req.Paramters.MaxCards))
+
+	request.URL.RawQuery = values.Encode()
+	client := &http.Client{}
+	res, err := client.Do(request)
+
+	// requestURL := fmt.Sprintf("https://api.pokemontcg.io/v2/cards%v", queryParams)
+	// res, err := http.Get(requestURL)
+	if err != nil {
+		return nil, err
 	}
 
-	return pokemonCards, nil
+	defer func() {
+		err := res.Body.Close()
+		if err != nil {
+			slog.ErrorContext(ctx, err.Error())
+		}
+	}()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("TCGAPI returned with status code: %v", res.StatusCode)
+	}
+
+	cardList := struct {
+		Data []model.PokemonCard `json:"data"`
+	}{}
+	if err := json.NewDecoder(res.Body).Decode(&cardList); err != nil {
+		return nil, err
+	}
+
+	// resBody, _ := getResponseBodyAsString(res.Body)
+
+	// var data Data
+	// err = json.Unmarshal([]byte(resBody), &data)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// var pokemonCards []model.PokemonCard
+	// for _, card := range data.cards {
+	// 	pokemonCards = append(pokemonCards, model.PokemonCard(*card))
+	// }
+
+	return cardList.Data, nil
+}
+
+func getResponseBodyAsString(responseBody io.ReadCloser) (string, error) {
+	buf := new(bytes.Buffer)
+	_, err := buf.ReadFrom(responseBody)
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
 
 func buildQuery(card model.Card) []string {
@@ -95,4 +174,32 @@ func buildQuery(card model.Card) []string {
 	}
 
 	return query
+}
+
+func buildQueryString(queryList []string) string {
+	//queryString := "?q="
+	var queryString string
+
+	for i, query := range queryList {
+		if i == 0 {
+			queryString = fmt.Sprintf("%v%v", queryString, query)
+		} else {
+			queryString = fmt.Sprintf("%v %v", queryString, query)
+		}
+
+	}
+
+	return queryString
+}
+
+func addQueryParameters(queryString string, parameters model.Parameters) string {
+	if parameters.OrderBy != "" {
+		queryString = fmt.Sprintf("%v&orderBy=%v", queryString, parameters.OrderBy)
+	}
+
+	if parameters.MaxCards != 0 {
+		queryString = fmt.Sprintf("%v&pageSize=%v", queryString, parameters.MaxCards)
+	}
+
+	return queryString
 }
