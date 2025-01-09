85c5d95f3c7883a147c42ed17ec208297572f31b
Joseph Nino de Guzman <61060613+JosephNinodG@users.noreply.github.com>
2025-01-09 23:40:17 +0000

    Update createtables data

--- sql/createtables.sql
+++ sql/createtables.sql
@@ -3,8 +3,8 @@ create table attack(
  "name"	varchar(50) NOT NULL,
  "cost" varchar(50) ARRAY NOT NULL,
  convertedEnergyCost integer NOT NULL,
-  damage varchar(50) NOT NULL,
-  description varchar(200) NOT NULL
+ damage varchar(50) NOT NULL,
+ description varchar(200) NOT NULL
 );
 
 create table images(
@@ -22,10 +22,27 @@ create table legalities(
  unlimited varchar(50) NOT NULL
 );
 
-create table traits(
+create table ancient_trait(
+ id serial primary key,
+ "name"	varchar(100),
+ "description" varchar(200)
+);
+
+create table ability(
  id serial primary key,
  "name"	varchar(100),
  "description" varchar(200),
+ "type" varchar(100)
+);
+
+create table weakness(
+ id serial primary key,
+ "type" varchar(100),
+ "value" varchar(100)
+);
+
+create table resistance(
+ id serial primary key,
  "type" varchar(100),
  "value" varchar(100)
 );
@@ -67,7 +84,7 @@ create table "card"(
   "rarity" varchar(50) NOT NULL,
   "flavorText" varchar(50) NOT NULL,
   "nationalPokedexNumbers" integer ARRAY NOT NULL,
-  ancientTrait_id integer references "traits" (id),
+  ancient_trait_id integer references "ancient_trait" (id),
   set_id integer references "set" (id),
   legalities_id integer references "legalities" (id),
   images_id integer references "images" (id)
@@ -75,25 +92,25 @@ create table "card"(
 
 create table "card_ability"(
 	card_id integer references card,
-  ability_id integer references traits,
+  ability_id integer references ability,
   primary key (card_id, ability_id)
 );
 
 create table "card_weakness"(
 	card_id integer references card,
-  weakness_id integer references traits,
+  weakness_id integer references weakness,
   primary key (card_id, weakness_id)
 );
 
 create table "card_resistance"(
 	card_id integer references card,
-  resistance_id integer references traits,
+  resistance_id integer references resistance,
   primary key (card_id, resistance_id)
 );
 
 create table "card_attack"(
 	card_id integer references card,
-  attack_id integer references traits,
+  attack_id integer references attack,
   primary key (card_id, attack_id)
 );
 
@@ -110,64 +127,61 @@ create table "collection_card"(
 );
 
 insert into "user" ("name", "email")
-values ('test1', 'test1@email.com')
+values ('test1', 'test1@email.com');
 
 insert into "attack" ("name", "cost", "convertedenergycost", "damage", "description")
-values ('attack1', '{"colorless","colorless"}', 2, '40', 'Placeholder description.')
-
-insert into "attack" ("name", "cost", "convertedenergycost", "damage", "description")
-values ('attack2', '{"Lightning","colorless","colorless"}', 3, '100', 'Placeholder description.')
+values ('attack1', '{"colorless","colorless"}', 2, '40', 'Placeholder description.'),
+('attack2', '{"Lightning","colorless","colorless"}', 3, '100', 'Placeholder description.');
 
 insert into "images" ("small", "large", "symbol", "logo")
-values ('card-smallimage1.png', 'card-largeimage1.png', null, null)
-
-insert into "images" ("small", "large", "symbol", "logo")
-values (null, null, 'set-symbol1.png', 'set-logo1.png')
-
-insert into "images" ("small", "large", "symbol", "logo")
-values ('card-smallimage2.png', 'card-largeimage2.png', null, null)
-
-insert into "images" ("small", "large", "symbol", "logo")
-values (null, null, 'set-symbol2.png', 'set-logo2.png')
+values ('card-smallimage1.png', 'card-largeimage1.png', null, null),
+(null, null, 'set-symbol1.png', 'set-logo1.png'),
+('card-smallimage2.png', 'card-largeimage2.png', null, null),
+(null, null, 'set-symbol2.png', 'set-logo2.png');
 
 insert into "legalities" ("standard", "expanded", "unlimited")
-values ('legal', 'legal', 'legal')
+values ('legal', 'legal', 'legal');
 
-insert into "traits" ("name", "description", "type", "value")
-values ('ancienttrait1', 'Placeholder description.', null, null),
-('ancienttrait2', 'Placeholder description.', null, null),
-('ability1', 'Placeholder description.', 'Poke-Power', null),
-('ability2', 'Placeholder description.', 'Ability', null),
-('ability3', 'Placeholder description.', 'Poké-Body', null),
-(null, 'weakness', 'Fighting', '+20'),
-(null, 'weakness', 'Water', 'x2'),
-(null, 'resistance', 'Metal', '-20')
+insert into "ancient_trait" ("name", "description")
+values ('ancienttrait1', 'Placeholder description.'),
+('ancienttrait2', 'Placeholder description.');
 
-insert into "sets" ("name", "series", "printedtotal", "total", "ptcgocode", "releasedate", "updateddate", "legalities_id", "images_id")
+insert into "ability" ("name", "description", "type")
+values ('ability1', 'Placeholder description.', 'Poke-Power'),
+('ability2', 'Placeholder description.', 'Ability'),
+('ability3', 'Placeholder description.', 'Poké-Body');
+
+insert into "weakness" ("type", "value")
+values ('Fighting', '+20'), ('Water', 'x2');
+
+insert into "resistance" ("type", "value")
+values ('Metal', '-20');
+
+insert into "set" ("name", "series", "printedtotal", "total", "ptcgocode", "releasedate", "updatedat", "legalities_id", "images_id")
 values ('set1','series1',100,150,'s1','2025/01/01','2025/01/01',1,2),
-('set2','series2',100,150,'s2','2024/01/01','2024/01/01',1,4)
+('set2','series2',100,150,'s2','2024/01/01','2024/01/01',1,4);
 
-insert into "card" ("name","supertype","subtypes","level","hp","types","evolvesfrom","evolvesto","rules","retreatcost","convertedretreatcost","number","artist","rarity","flavorText","nationalPokedexNumbers","ancienttrait_id","set_id","legalities_id","images_id")
+insert into "card" ("name","supertype","subtypes","level","hp","types","evolvesfrom","evolvesto","rules","retreatcost","convertedretreatcost","number","artist","rarity","flavorText","nationalPokedexNumbers","ancient_trait_id","set_id","legalities_id","images_id")
 values ('card1','Pokemon','{"Basic"}',null,'50','{"Lightning"}',null,'{"card2"}','{"Placeholder rules."}','{"Colorless","Colorless"}',2,1,'artist','Uncommon','Placeholder text.','{1}',null,1,1,1),
-('card2','Pokemon','{"Stage 1"}',null,'100','{"Lightning"}','card1','{}','{"Placeholder rules."}','{"Lightning","Colorless","Colorless"}',3,2,'artist','Rare','Placeholder text.','{2}',null,1,1,2)
+('card2','Pokemon','{"Stage 1"}',null,'100','{"Lightning"}','card1','{}','{"Placeholder rules."}','{"Lightning","Colorless","Colorless"}',3,2,'artist','Rare','Placeholder text.','{2}',null,1,1,2);
 
 insert into "card_ability" ("card_id", "ability_id")
-values (2, 4)
+values (2, 2);
 
 insert into "card_attack" ("card_id", "attack_id")
-values (1, 1), (2, 2) 
+values (1, 1), (2, 2);
 
 insert into "card_resistance" ("card_id", "resistance_id")
-values (1, 8), (2, 8)
+values (1, 1), (2, 1);
 
 insert into "card_weakness" ("card_id", "weakness_id")
-values (1, 6), (2, 6)
+values (1, 1), (2, 1);
 
 insert into "collection" ("name", "user_id")
-values ("testcollection1", 1)
+values ('testcollection1', 1);
 
 insert into "collection_card" ("card_id", "collection_id")
-values (1, 1), (2, 1)
+values (1, 1), (2, 1);
 
 -- example JOIN:
 SELECT c as card, a as attack from card c join card_attack ca on ca.card_id = c.id join attack a on a.id = ca.attack_id
