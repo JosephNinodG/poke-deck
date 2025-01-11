c56d8fdf2d312abecbf8c1b41739d2839b12a414
Joseph Nino de Guzman <61060613+JosephNinodG@users.noreply.github.com>
2025-01-11 18:07:28 +0000

    PD-4: Update table creation queries and store select card and card collection queries

--- sql/createtables.sql
+++ sql/createtables.sql
@@ -2,24 +2,35 @@ create table attack(
  id serial primary key,
  "name"	varchar(50) NOT NULL,
  "cost" varchar(50) ARRAY NOT NULL,
- convertedEnergyCost integer NOT NULL,
- damage varchar(50) NOT NULL,
- description varchar(200) NOT NULL
+ "convertedEnergyCost" integer NOT NULL,
+ "damage" varchar(50) NOT NULL,
+ "description" varchar(200) NOT NULL
 );
 
-create table images(
+create table set_images(
  id serial primary key,
- small	varchar(100),
- "large" varchar(100),
  "symbol" varchar(100),
  "logo" varchar(100)
 );
 
-create table legalities(
+create table set_legalities(
  id serial primary key,
- standard	varchar(50) NOT NULL,
- expanded varchar(50) NOT NULL,
- unlimited varchar(50) NOT NULL
+ "standard"	varchar(50) NOT NULL,
+ "expanded" varchar(50) NOT NULL,
+ "unlimited" varchar(50) NOT NULL
+);
+
+create table card_images(
+ id serial primary key,
+ "small"	varchar(100),
+ "large" varchar(100)
+);
+
+create table card_legalities(
+ id serial primary key,
+ "standard"	varchar(50) NOT NULL,
+ "expanded" varchar(50) NOT NULL,
+ "unlimited" varchar(50) NOT NULL
 );
 
 create table ancient_trait(
@@ -57,28 +68,28 @@ create table "set"(
  id serial primary key,
  "name"	varchar(200) NOT NULL,
  "series" varchar(50) NOT NULL,
-  printedTotal integer NOT NULL,
-  total integer NOT NULL,
-  ptcgoCode varchar(50) NOT NULL,
-  releaseDate varchar(50) NOT NULL,
-  updatedAt varchar(50) NOT NULL,
-  legalities_id integer references "legalities" (id),
-  images_id integer references "images" (id)
+  "printedTotal" integer NOT NULL,
+  "total" integer NOT NULL,
+  "ptcgoCode" varchar(50) NOT NULL,
+  "releaseDate" varchar(50) NOT NULL,
+  "updatedAt" varchar(50) NOT NULL,
+  set_legalities_id integer references "set_legalities" (id),
+  set_images_id integer references "set_images" (id)
 );
 
 create table "card"(
  id serial primary key,
  "name"	varchar(200) NOT NULL,
- 	supertype varchar(50) NOT NULL,
-  subtypes varchar(100) ARRAY NOT NULL,
+ 	"supertype" varchar(50) NOT NULL,
+  "subtypes" varchar(100) ARRAY NOT NULL,
   "level" varchar(50),
   "hp" varchar(50) NOT NULL,
   "types" varchar(100) ARRAY NOT NULL,
-  evolvesFrom varchar(100),
-  evolvesTo varchar(100) ARRAY NOT NULL,
-  rules varchar(200) ARRAY NOT NULL,
-  retreatCost varchar(50) ARRAY NOT NULL,
-  convertedRetreatCost integer NOT NULL,
+  "evolvesFrom" varchar(100),
+  "evolvesTo" varchar(100) ARRAY NOT NULL,
+  "rules" varchar(200) ARRAY NOT NULL,
+  "retreatCost" varchar(50) ARRAY NOT NULL,
+  "convertedRetreatCost" integer NOT NULL,
   "number" varchar(50) NOT NULL,
   "artist" varchar(50) NOT NULL,
   "rarity" varchar(50) NOT NULL,
@@ -86,8 +97,8 @@ create table "card"(
   "nationalPokedexNumbers" integer ARRAY NOT NULL,
   ancient_trait_id integer references "ancient_trait" (id),
   set_id integer references "set" (id),
-  legalities_id integer references "legalities" (id),
-  images_id integer references "images" (id)
+  card_legalities_id integer references "card_legalities" (id),
+  card_images_id integer references "card_images" (id)
 );
 
 create table "card_ability"(
@@ -129,17 +140,22 @@ create table "collection_card"(
 insert into "user" ("name", "email")
 values ('test1', 'test1@email.com');
 
-insert into "attack" ("name", "cost", "convertedenergycost", "damage", "description")
+insert into "attack" ("name", "cost", "convertedEnergyCost", "damage", "description")
 values ('attack1', '{"colorless","colorless"}', 2, '40', 'Placeholder description.'),
 ('attack2', '{"Lightning","colorless","colorless"}', 3, '100', 'Placeholder description.');
 
-insert into "images" ("small", "large", "symbol", "logo")
-values ('card-smallimage1.png', 'card-largeimage1.png', null, null),
-(null, null, 'set-symbol1.png', 'set-logo1.png'),
-('card-smallimage2.png', 'card-largeimage2.png', null, null),
-(null, null, 'set-symbol2.png', 'set-logo2.png');
+insert into "set_images" ("symbol", "logo")
+values ('set-symbol1.png', 'set-logo1.png'),
+('set-symbol2.png', 'set-logo2.png');
 
-insert into "legalities" ("standard", "expanded", "unlimited")
+insert into "set_legalities" ("standard", "expanded", "unlimited")
+values ('legal', 'legal', 'legal');
+
+insert into "card_images" ("small", "large")
+values ('card-smallimage1.png', 'card-largeimage1.png'),
+('card-smallimage2.png', 'card-largeimage2.png');
+
+insert into "card_legalities" ("standard", "expanded", "unlimited")
 values ('legal', 'legal', 'legal');
 
 insert into "ancient_trait" ("name", "description")
@@ -157,11 +173,11 @@ values ('Fighting', '+20'), ('Water', 'x2');
 insert into "resistance" ("type", "value")
 values ('Metal', '-20');
 
-insert into "set" ("name", "series", "printedtotal", "total", "ptcgocode", "releasedate", "updatedat", "legalities_id", "images_id")
-values ('set1','series1',100,150,'s1','2025/01/01','2025/01/01',1,2),
-('set2','series2',100,150,'s2','2024/01/01','2024/01/01',1,4);
+insert into "set" ("name", "series", "printedTotal", "total", "ptcgoCode", "releaseDate", "updatedAt", "set_legalities_id", "set_images_id")
+values ('set1','series1',100,150,'s1','2025/01/01','2025/01/01',1,1),
+('set2','series2',100,150,'s2','2024/01/01','2024/01/01',1,2);
 
-insert into "card" ("name","supertype","subtypes","level","hp","types","evolvesfrom","evolvesto","rules","retreatcost","convertedretreatcost","number","artist","rarity","flavorText","nationalPokedexNumbers","ancient_trait_id","set_id","legalities_id","images_id")
+insert into "card" ("name","supertype","subtypes","level","hp","types","evolvesFrom","evolvesTo","rules","retreatCost","convertedRetreatCost","number","artist","rarity","flavorText","nationalPokedexNumbers","ancient_trait_id","set_id","card_legalities_id","card_images_id")
 values ('card1','Pokemon','{"Basic"}',null,'50','{"Lightning"}',null,'{"card2"}','{"Placeholder rules."}','{"Colorless","Colorless"}',2,1,'artist','Uncommon','Placeholder text.','{1}',null,1,1,1),
 ('card2','Pokemon','{"Stage 1"}',null,'100','{"Lightning"}','card1','{}','{"Placeholder rules."}','{"Lightning","Colorless","Colorless"}',3,2,'artist','Rare','Placeholder text.','{2}',null,1,1,2);
 
@@ -182,6 +198,3 @@ values ('testcollection1', 1);
 
 insert into "collection_card" ("card_id", "collection_id")
 values (1, 1), (2, 1);
-
--- example JOIN:
-SELECT c as card, a as attack from card c join card_attack ca on ca.card_id = c.id join attack a on a.id = ca.attack_id
